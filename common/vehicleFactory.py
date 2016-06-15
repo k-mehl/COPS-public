86d1d8def1f8f613e249e57a36f9a5f054a1a8ac
Aleksandar Trifunovic <a.trifunovic@tu-braunschweig.de>
2016-06-15 16:07:49 +0200

    docs to sphinx docs (google style)

--- common/vehicleFactory.py
+++ common/vehicleFactory.py
@@ -2,18 +2,24 @@ import random
 import sumolib
 import os
 
-## Generate a random demand of vehicles for the parking space search and create
-#  the corresponding XML file
-#  @param p_num Number of vehicles
-#  @param p_resourcedir Base path name for SUMO related resources
-#  @param p_edgefile Filename to read edges, default: reroute.edg.xml
-#  @param p_routefile Filename to write routes, default: reroute.rou.xml
-def generatePsvDemand(p_num, p_resourcedir="", p_routefile="reroute.rou.xml", p_edgefile="reroute.edg.xml", ):
+
+def generatePsvDemand(p_num, p_resourcedir="", p_routefile="reroute.rou.xml",
+                      p_edgefile="reroute.edg.xml"):
+    """ Generate a random demand of vehicles for the parking space search and
+    create the corresponding XML file
+
+    Args:
+        p_num (int): Number of vehicles
+        p_resourcedir (str): Base path name for SUMO related resources
+        p_edgefile (str):  Filename to read edges, default: reroute.edg.xml
+        p_routefile (str): Filename to write routes, default: reroute.rou.xml
+    """
     # create seperate lists for possible origin or destination edges
     origins = []
     destinations = []
     # parse edges XML file for all network edges
-    for edge in sumolib.output.parse(os.path.join(p_resourcedir, p_edgefile), ['edge']):
+    for edge in sumolib.output.parse(os.path.join(p_resourcedir, p_edgefile),
+                                     ['edge']):
         # if the edge ID contains "entry": use it as origin only;
         # otherwise: use it as destination only
         if "entry" in str(edge.id):
@@ -23,13 +29,13 @@ def generatePsvDemand(p_num, p_resourcedir="", p_routefile="reroute.rou.xml", p_
 
     # open routes XML file for write access
     f = open(os.path.join(p_resourcedir, p_routefile), 'w')
-    
+
     # define tab as 4 spaces
     tab = "    "
     # base string for vehicle trip data
     veh = '<trip id="veh{0}" depart="0" from="{1}" to="{2}" type="Car" ' + \
             'color="0,1,0"/>\n'
-    
+
     # write header
     f.write("<vehicles>\n")
     # write definitions of 'Car' type
