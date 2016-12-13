483e546c457142d4a18c5a3eea60c03fac84ce72
Aleksandar Trifunovic <a.trifunovic@tu-braunschweig.de>
2016-12-13 22:39:07 +0100

    doc formatting

--- env/parkingSpace.py
+++ env/parkingSpace.py
@@ -1,14 +1,18 @@
 #!usr/bin/env python
 from __future__ import print_function
 
-class ParkingSpace(object):
 
-    ## Constructor for parking spaces, initializes parking space attributes
-    #  @param Name
-    #  @param edgeID Specifies the edge on which the parking space is located
-    #  @param position Specifies the position of the parking space on the edge
-    #                  [meters]
+class ParkingSpace(object):
     def __init__(self, name, edgeID, position, available=False):
+        """ Constructor for parking spaces, initializes parking space
+        attributes
+
+        Args:
+            name: Parking space indentifier.
+            edgeID: Specifies the edge on which the parking space is located.
+            position: Specifies the position of the parking space on the edge
+                in meters.
+        """
         self.name = name
         # parking space is not available by default
         self.available = available
@@ -17,30 +21,35 @@ class ParkingSpace(object):
         # parking space is not assigned to any vehicle by default
         self.assignedToVehicleID = ""
 
-    ## Check for equivalence by name attribute
     def __eq__(self, other):
+        """ Check for equivalence by name attribute """
         return self.name == other.name
 
-    ## Assign a parking space to a specific vehicle
-    #  @param vehID Identifier of the vehicle which has found the parking space
     def assignToVehicle(self, vehID):
-        # ensure that the parking space is no longer available to other 
+        """ Assign a parking space to a specific vehicle
+
+        Args:
+            vehID: Identifier of the vehicle which has found the parking space
+        """
+        # ensure that the parking space is no longer available to the other
         # vehicles
         self.available = False
         self.assignedToVehicleID = vehID
 
-    ## Unassign a parking space from a specific vehicle 
-    #  (mainly for future variants with dynamic occupancy)
-    #  @return Return the ID of the vehicle which has 
-    #          vacated this parking space
     def unassign(self):
+        """ Unassign a parking space from a specific vehicle (mainly for future
+        variants with dynamic occupancy)
+
+        Returns:
+            The ID of the vehicle which has vacated this parking space.
+        """
         self.available = True
         vehID = self.assignedToVehicleID
         self.assignedToVehicleID = ""
         return vehID
 
-                
+
 if __name__ == "__main__":
     print("Nothing to do.")
-#else:
-    #print("ParkingSpace class imported.")
+# else:
+#     print("ParkingSpace class imported.")
