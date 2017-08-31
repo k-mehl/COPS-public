345c47fbf5b5fdedb654ffba5669abaefcf4b412
Aleksandar Trifunovic <a.trifunovic@tu-braunschweig.de>
2017-08-31 22:51:05 +0200

    main: Aliasing variables

--- main.py
+++ main.py
@@ -69,8 +69,9 @@ if __name__ == "__main__":
     l_config = configuration.Configuration(l_args, l_configdir)
 
     print("* pre-testing runcfg for all runs")
+    sim_conf = l_config.getCfg("simulation")
     if l_config.existRunCfg():
-        tmp_iter = xrange(l_config.getCfg("simulation").get("runs"))
+        tmp_iter = xrange(sim_conf.get("runs"))
         if [run for run in tmp_iter if not l_config.isRunCfgOk(run)]:
             raise BaseException("Error(s) in run configuration")
         else:
@@ -92,11 +93,11 @@ if __name__ == "__main__":
     l_maxTimeSum = 0
     l_maxDistanceSum = 0.0
 
-    l_numParkingSpaces = l_config.getCfg("simulation").get("parkingspaces")['free']
-    l_numVehicles = l_config.getCfg("simulation").get("vehicles")
-    l_numCoopPhase2 = l_config.getCfg("simulation").get("coopratioPhase2")
-    l_numCoopPhase3 = l_config.getCfg("simulation").get("coopratioPhase3")
-    l_numRuns = l_config.getCfg("simulation").get("runs")
+    l_numParkingSpaces = sim_conf.get("parkingspaces")['free']
+    l_numVehicles = sim_conf.get("vehicles")
+    l_numCoopPhase2 = sim_conf.get("coopratioPhase2")
+    l_numCoopPhase3 = sim_conf.get("coopratioPhase3")
+    l_numRuns = sim_conf.get("runs")
     l_externalPlanned = l_config.getCfg("vehicle").get("weights").get("coop").get("externalplanned")
     l_externalVisit = l_config.getCfg("vehicle").get("weights").get("coop").get("externalvisit")
     l_selfVisit = l_config.getCfg("vehicle").get("weights").get("coop").get("selfvisit")
@@ -108,7 +109,7 @@ if __name__ == "__main__":
     if not os.path.isdir(l_mainresultdir):
         os.mkdir(l_mainresultdir)
 
-    if l_config.getCfg("simulation").get("resulttimestamped"):
+    if sim_conf.get("resulttimestamped"):
         l_resultdir = time.strftime("%Y%m%d%H%M%S")
         if not os.path.isdir((os.path.join(l_mainresultdir, l_resultdir))):
             os.mkdir((os.path.join(l_mainresultdir, l_resultdir)))
@@ -140,7 +141,7 @@ if __name__ == "__main__":
         "-ev" + str(l_externalVisit) + \
         "-sv" + str(l_selfVisit) + ".csv"
 
-    if l_config.getCfg("simulation").get("resulttimestamped"):
+    if sim_conf.get("resulttimestamped"):
         rf = open(os.path.join(l_mainresultdir, l_resultdir, l_resultfile), 'w')
         cf = open(os.path.join(l_mainresultdir, l_resultdir, l_convergencefile), 'w')
     else:
@@ -156,10 +157,10 @@ if __name__ == "__main__":
              "avgMaxSearchDist\n")
 
     l_successfulruns = True
-    for i_run in xrange(l_config.getCfg("simulation").get("runs")):
+    for i_run in xrange(sim_conf.get("runs")):
         print("PID", os.getpid(), "RUNCFG:",
-              l_config.getCfg("simulation").get("runconfiguration"),
-              "RUN:", i_run+1, "of", l_config.getCfg("simulation").get("runs"))
+              sim_conf.get("runconfiguration"),
+              "RUN:", i_run+1, "of", sim_conf.get("runs"))
         try:
             l_successes, l_searchTimes, l_walkingTimes, l_searchDistances, \
                     l_walkingDistances, l_searchPhases = l_runtime.run(i_run)
@@ -167,7 +168,7 @@ if __name__ == "__main__":
             for i_result in range(len(l_searchTimes)):
                 rf.write(str(l_numVehicles) + ",")
                 rf.write(str(l_numParkingSpaces) + ",")
-                rf.write(str(i_run+1) + ",")
+                rf.write(str(i_run + 1) + ",")
                 rf.write(str(l_numCoopPhase2) + ",")
                 rf.write(str(l_numCoopPhase3) + ",")
                 rf.write(str(l_searchTimes[i_result]) + ",")
@@ -223,36 +224,36 @@ if __name__ == "__main__":
             # cleanup open file streams and write run cfg if not exists
             rf.close()
             cf.close()
-            if not os.path.isfile(l_config.getCfg("simulation").get("runconfiguration")):
+            if not os.path.isfile(sim_conf.get("runconfiguration")):
                 l_config.writeRunCfg()
             else:
                 print("There exists a run cfg at {}! Refusing to overwrite it!".format(
-                    os.path.isfile(l_config.getCfg("simulation").get("runconfiguration"))))
+                    os.path.isfile(sim_conf.get("runconfiguration"))))
             raise BaseException("/!\\ Unhandled exception in run id {} occurred /!\\".format(i_run))
 
     rf.close()
     cf.close()
 
     # write run cfg - make sure not to overwrite an existing one
-    if not os.path.isfile(l_config.getCfg("simulation").get("runconfiguration")):
+    if not os.path.isfile(sim_conf.get("runconfiguration")):
         l_config.writeRunCfg()
     else:
         print("There exists a run cfg at {}! Refusing to overwrite it!".format(
-            os.path.isfile(l_config.getCfg("simulation").get("runconfiguration"))))
+            os.path.isfile(sim_conf.get("runconfiguration"))))
 
-    if l_config.getCfg("simulation").get("resulttimestamped"): 
+    if sim_conf.get("resulttimestamped"): 
         sf = open(os.path.join(l_mainresultdir, l_resultdir, l_summaryfile), 'w')
     else:
         sf = open(os.path.join(l_mainresultdir, l_summaryfile), 'w')
 
-    l_successRate = 100*l_successesSum/(l_config.getCfg("simulation").get("runs")*l_config.getCfg("simulation").get("vehicles"))
+    l_successRate = 100 * l_successesSum/(sim_conf.get("runs") * sim_conf.get("vehicles"))
 
     sf.write("")
     sf.write("==== SUMMARY AFTER " + str(l_numRuns) + " RUNS ====\n")
     sf.write("parameters:          " + str(l_numVehicles) + " vehicles\n")
     sf.write("                     " + str(l_numParkingSpaces) + " parking spaces\n")
-    sf.write("                     " + str(int(l_numCoopPhase2*100)) + " percent of drivers cooperate in phase 2\n")
-    sf.write("                     " + str(int(l_numCoopPhase3*100)) + " percent of drivers cooperate in phase 3\n")
+    sf.write("                     " + str(int(l_numCoopPhase2 * 100)) + " percent of drivers cooperate in phase 2\n")
+    sf.write("                     " + str(int(l_numCoopPhase3 * 100)) + " percent of drivers cooperate in phase 3\n")
     if l_successesSum:
         l_searchTimesAvg = l_searchTimesSum / float(l_successesSum)
         l_walkingTimesAvg = l_walkingTimesSum / float(l_successesSum)
@@ -262,8 +263,8 @@ if __name__ == "__main__":
         l_totalDistancesAvg = l_totalDistancesSum / float(l_successesSum)
         l_parkedInPhase2Rate = l_parkedInPhase2Sum / float(l_successesSum)
         l_parkedInPhase3Rate = l_parkedInPhase3Sum / float(l_successesSum)
-        l_maxTimeAvg = l_maxTimeSum / float(i_run+1)
-        l_maxDistAvg = l_maxDistanceSum / float(i_run+1)
+        l_maxTimeAvg = l_maxTimeSum / float(i_run + 1)
+        l_maxDistAvg = l_maxDistanceSum / float(i_run + 1)
         sf.write("avg search time      " + str(l_searchTimesAvg) + " seconds\n")
         sf.write("avg walking time     " + str(l_walkingTimesAvg) + " seconds\n")
         sf.write("AVG TOTAL TIME       " + str(l_totalTimesAvg) + " SECONDS\n")
