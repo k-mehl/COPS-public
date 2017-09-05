72d204c09f94011117f554d868e20dec203bc86b
Aleksandar Trifunovic <a.trifunovic@tu-braunschweig.de>
2017-09-06 00:27:28 +0200

    add function to compute communication group i.e. the ones that share all info at some specific point in time

--- parking/runtime/communication.py
+++ parking/runtime/communication.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def mark_neighbours(vehicles, distance):
     """ Discovers neighbors for a vehicles and saves them internally into
     vehicle objects. Has complexity of approx O(len(vehicles)**2/2).
@@ -13,18 +15,48 @@ def mark_neighbours(vehicles, distance):
     for v in vehicles:
         v.connected_neighbors = []
 
-    # loop over vehicles but in other loop don't check vehicles that went
+    # loop over vehicles but in inner loop don't check vehicles that went
     # through first loop because connections are symmetric
     for veh_idx, veh in list(enumerate(vehicles))[:-1]:
         for other in vehicles[veh_idx + 1:]:
             veh_x, veh_y = veh.position
             oth_x, oth_y = other.position
             if (abs(veh_x - oth_x) + abs(veh_y - oth_y)) < distance:
-                veh.connected_neighbors.append(other.name)
-                other.connected_neighbors.append(veh.name)
+                veh.connected_neighbors.append(other)
+                other.connected_neighbors.append(veh)
 
-# function to create a graph of connected groups i.e. if veh0 is connected to
-# veh1 and veh1 is connected to veh2 then all 3 vehicles share same info about
-# parking spaces
 def communication_groups(vehicles):
-    raise NotImplementedError
+    """ Creates groups of vehicles that are connected at specific time step
+    with an assumption that communication is instantaneous. For example if
+    vehicle_0 is connected to vehicle_1 and vehicle_1 is connected to vehicle_2
+    then all 3 vehicles share same knowledge about parking spaces.
+
+    Args:
+        vehicles (list): A list with vehicle objects.
+
+    Returns:
+        list(list): A list of list where each inner list is a group of vehicles
+            that share the same knowledge at that time instance
+    """
+    vehicles = vehicles[:]
+
+    groups = []
+    while vehicles:
+        group = []
+        veh_queue = deque([vehicles[0]])
+
+        while veh_queue:
+            veh = veh_queue.pop()
+            group.append(veh)
+
+            # don't add to queue vehicles that are already in the group or are
+            # already in the queue
+            filtered = (v for v in veh.connected_neighbors
+                        if (v not in group) and (v not in veh_queue))
+            veh_queue.extend(filtered)
+
+            if vehicles:
+                vehicles.remove(veh)
+
+        groups.append(group)
+    return groups
--- tests/test_communication.py
+++ tests/test_communication.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("../parking")
 
 from parking.runtime.communication import mark_neighbours
+from parking.runtime.communication import communication_groups
 
 # Proper vehicle class is harder to use hence a quick mock
 class Vehicle():
@@ -36,3 +37,33 @@ def test_mark_neighbours():
     assert len(vehicles[1].connected_neighbors) == 2
     assert len(vehicles[2].connected_neighbors) == 2
     assert len(vehicles[-1].connected_neighbors) == 0
+
+def test_communication_groups():
+    # reset name number to 0 and set all cords to 0 since they are not relevant
+    Vehicle.num = 0
+    veh0 = Vehicle((0, 0))
+    veh1 = Vehicle((0, 0))
+    veh2 = Vehicle((0, 0))
+    veh3 = Vehicle((0, 0))
+    veh4 = Vehicle((0, 0))
+    veh5 = Vehicle((0, 0))
+
+    veh0.connected_neighbors = [veh1]
+    veh1.connected_neighbors = [veh0]
+
+    veh3.connected_neighbors = [veh4, veh5]
+    veh4.connected_neighbors = [veh3, veh5]
+    veh5.connected_neighbors = [veh3, veh4]
+
+    res = communication_groups([veh0, veh1, veh2, veh3, veh4, veh5])
+
+    assert len(res) == 3
+
+    assert len(res[0]) == 2
+    assert (veh0 and veh1) in res[0]
+
+    assert len(res[1]) == 1
+    assert veh2 in res[1]
+
+    assert len(res[2]) == 3
+    assert (veh3 and veh4 and veh5) in res[2]
